cd62363e0c1c74b2f2576821a0430bb2bce508e5
GurutzLarrea <gurutzll@gmail.com>
2025-03-07 15:23:29 +0100

    Cambios horarios y actualizacion JSON para horarios, aún no terminado

--- data/data.js
+++ data/data.js
@@ -1,61 +1,46 @@
 //funcion auxiliar para parsear de html a JSON, ahora mismo no esta activa, esto genera el dato eventos en JSON
-const htmlToJsonEventos = () =>{
-    const events = document.getElementById("events");
-    const articles = events.children;
+// const htmlToJsonEventos = () =>{
+//     const events = document.getElementById("events");
+//     const articles = events.children;
 
-    const arrayCollection = Array.from(articles);
-    const eventos = [];
-    const regex = /<img src="(.*)"\s[\s\S]*<h2>(.*)<\/h2>[\s\S]*<p>(.*)<\/p>[\s\S]*href="(.*)"/g;
-    arrayCollection.forEach(element => {
+//     const arrayCollection = Array.from(articles);
+//     const eventos = [];
+//     const regex = /<img src="(.*)"\s[\s\S]*<h2>(.*)<\/h2>[\s\S]*<p>(.*)<\/p>[\s\S]*href="(.*)"/g;
+//     arrayCollection.forEach(element => {
 
-        const htmlString = element.innerHTML;
+//         const htmlString = element.innerHTML;
 
-        console.log(htmlString);
+//         console.log(htmlString);
 
-        while((match = regex.exec(htmlString) ) !== null){
-            eventos.push({
-                img: match[1],
-                titulo: match[2],
-                fecha: match[3],
-                link: match[4]
-            })
-        }  
-    }); 
-}
+//         while((match = regex.exec(htmlString) ) !== null){
+//             eventos.push({
+//                 img: match[1],
+//                 titulo: match[2],
+//                 fecha: match[3],
+//                 link: match[4]
+//             })
+//         }  
+//     }); 
+// }
 
 //htmlToJson();
 
 
-
-const datosEventos = [
-    
-    {
-        "img": "assets/img/practica-lademoswing.png",
-        "titulo": "Práctica Swing en La Demo",
-        "fecha": "Viernes 7 de Marzo de 21:00 a 22:30"
-        
-    },
-    {
-        "img": "assets/img/mañaneo-lademoswing.png",
-        "titulo": "Mañaneo en La Demo",
-        "fecha": "Sábado 22 de Marzo de 11:00 a 14:30",
-        "link": "https://docs.google.com/forms/d/1LxY4rw-r-DDso24xbyDrnc6ODxbJFXyMt6Mg9hoopaA/viewform?edit_requested=true"
-    },
-    {
-        "img": "assets/img/mañaneo-lademoswing.png",
-        "titulo": "Mañaneo en La Demo",
-        "fecha": "Sábado 5 de abril de 11:00 a 14:30"
-    },
-    {
-        "img": "assets/img/practica-lademoswing.png",
-        "titulo": "Práctica Swing en La Demo",
-        "fecha": "Sábado 25 de Abril de 11:00 a 14:30"
-    }
-]
-
-
 //generar HTML
-const generarHtml =()=>{
+fetch('./data/eventos.json') // Cargar el archivo JSON
+.then(response => {
+    if (!response.ok) {
+        throw new Error(`Error HTTP! Estado: ${response.status}`);
+    }
+    return response.json(); // Convertir respuesta a objeto JS
+})
+.then(datosEventos => {
+    console.log("JSON cargado:", datosEventos);
+    generarHtml(datosEventos);   
+})  
+
+
+const generarHtml =(datosEventos)=>{
     const containerEventos = document.getElementById("events");
     /* const datosEventos = require("./eventos.json");*/
     datosEventos.forEach((evento)=>{
@@ -86,34 +71,5 @@ const generarHtml =()=>{
     
 }
 
-    /* fetch('./eventos.json') // Cargar el archivo JSON
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error(`Error HTTP! Estado: ${response.status}`);
-                }
-                return response.json(); // Convertir respuesta a objeto JS
-            })
-            .then(data => {
-                console.log("JSON cargado:", data);
 
-                datosEventos.forEach((evento)=>{
-                    const article = document.createElement("article")
-                   // article.classList.add("events-row");
-                    const eventoHtml = `
-                        <div class="image">
-                            <img src="${evento.img}" alt="">
-                        </div>
-                        <div class="text">
-                            <h2>${evento.titulo}</h2>
-                            <div class="divider"></div>
-                            <p>${evento.fecha}</p>
-                        </div>
-                    `;
-                    article.innerHTML=eventoHtml;
-                    containerEventos.appendChild(article);
-                })
-            }) */  
     
-generarHtml();
-
-
--- data/dataHorarios.js
+++ data/dataHorarios.js
@@ -1,75 +1,45 @@
-const datosHorarios =[
-    {
-        "dia": "Lunes",
-        "clases": [
-            {   
-                "time":"",
-                "name":"",
-                "level":"",
-                "teacher":"",
-                "link": "https://forms.gle/8vadrVYQGSY9q4KdA"
-            },
-            {
-                "time":"18:00-19:00 - Sala Bambú Danza",
-                "name":"Solo Jazz",
-                "level":"Iniciación",
-                "teacher":"Julie",
-                "link": "https://forms.gle/8vadrVYQGSY9q4KdA"
-            },
-            {
-                "time":"18:00-19:00 - Sala Bambú Danza",
-                "name":"Solo Jazz",
-                "level":"Iniciación",
-                "teacher":"Julie",
-                "link": "https://forms.gle/8vadrVYQGSY9q4KdA" 
-            },
-            {
-                "time":"18:00-19:00 - Sala Bambú Danza",
-                "name":"Solo Jazz",
-                "level":"Iniciación",
-                "teacher":"Julie",
-                "link": "https://forms.gle/8vadrVYQGSY9q4KdA"
-            },
-        ],
-        "dia": "Martes",
-        "clases": [
-            {   
-                "time":"",
-                "name":"",
-                "level":"",
-                "teacher":"",
-                "link": "https://forms.gle/8vadrVYQGSY9q4KdA"
-            },
-            {
-                "time":"18:00-19:00 - Sala Bambú Danza",
-                "name":"Solo Jazz",
-                "level":"Iniciación",
-                "teacher":"Julie",
-                "link": "https://forms.gle/8vadrVYQGSY9q4KdA"
-            },
-            {
-                "time":"18:00-19:00 - Sala Bambú Danza",
-                "name":"Solo Jazz",
-                "level":"Iniciación",
-                "teacher":"Julie",
-                "link": "https://forms.gle/8vadrVYQGSY9q4KdA" 
-            },
-            {
-                "time":"18:00-19:00 - Sala Bambú Danza",
-                "name":"Solo Jazz",
-                "level":"Iniciación",
-                "teacher":"Julie",
-                "link": "https://forms.gle/8vadrVYQGSY9q4KdA"
-            },
-        ], 
+/* fetch('./data/horario.json') // Cargar el archivo JSON
+.then(response => {
+    if (!response.ok) {
+        throw new Error(`Error HTTP! Estado: ${response.status}`);
     }
-]
-
+    return response.json(); // Convertir respuesta a objeto JS
+})
+.then(datosHorarios => {
+    console.log("JSON cargado:", datosHorarios);
+    generarHtmlHorarios(datosHorarios);   
+})  
 
 //generar HTMl de horarios
-generarHtmlHorarios = () =>{
+generarHtmlHorarios = (datosHorarios) =>{
+
+    
+
+    const horasArray = new Set();
+    for(const dia in datosHorarios){
+        //console.log(datosHorarios[dia]);
+        datosHorarios[dia].clases.forEach((clase)=>{
+            horasArray.add(clase.time);
+        })
+    }
+    //console.log(horasArray);
+
+
     const containerHorarios = document.getElementById("tablaHorarios");
-    datosHorarios.forEach((dia)=>{
-        
-    })
+    horasArray.forEach((hora)=>{
+        const row = document.createElement("tr");
+        const celdaHora = document.createElement("th");
+        celdaHora.textContent = hora;
+        row.appendChild(celdaHora);
+        for(const dia in datosHorarios){
+            const celda = document.createElement("td");
+            const claseName = datosHorarios[dia].clases.find((clase)=> clase.time === hora)  
+            celda.textContent = claseName;            
+            row.appendChild(celda);
+        }
+        containerHorarios.appendChild(row);
+    });
+
+
 }
+ */
