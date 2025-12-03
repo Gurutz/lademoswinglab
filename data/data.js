312ba948251920bea2c8b17235e3388be56e5662
GurutzLarrea <gurutzll@gmail.com>
2025-12-03 18:41:04 +0100

    Actualización grande

--- data/data.js
+++ data/data.js
@@ -1,46 +1,55 @@
 //funcion auxiliar para parsear de html a JSON, ahora mismo no esta activa, esto genera el dato eventos en JSON
-// const htmlToJsonEventos = () =>{
-//     const events = document.getElementById("events");
-//     const articles = events.children;
+const htmlToJsonEventos = () =>{
+    const events = document.getElementById("events");
+    const articles = events.children;
 
-//     const arrayCollection = Array.from(articles);
-//     const eventos = [];
-//     const regex = /<img src="(.*)"\s[\s\S]*<h2>(.*)<\/h2>[\s\S]*<p>(.*)<\/p>[\s\S]*href="(.*)"/g;
-//     arrayCollection.forEach(element => {
+    const arrayCollection = Array.from(articles);
+    const eventos = [];
+    const regex = /<img src="(.*)"\s[\s\S]*<h2>(.*)<\/h2>[\s\S]*<p>(.*)<\/p>[\s\S]*href="(.*)"/g;
+    arrayCollection.forEach(element => {
 
-//         const htmlString = element.innerHTML;
+        const htmlString = element.innerHTML;
 
-//         console.log(htmlString);
+        console.log(htmlString);
 
-//         while((match = regex.exec(htmlString) ) !== null){
-//             eventos.push({
-//                 img: match[1],
-//                 titulo: match[2],
-//                 fecha: match[3],
-//                 link: match[4]
-//             })
-//         }  
-//     }); 
-// }
+        while((match = regex.exec(htmlString) ) !== null){
+            eventos.push({
+                img: match[1],
+                titulo: match[2],
+                fecha: match[3],
+                link: match[4]
+            })
+        }  
+    }); 
+}
 
 //htmlToJson();
 
 
-//generar HTML
-fetch('./data/eventos.json') // Cargar el archivo JSON
-.then(response => {
-    if (!response.ok) {
-        throw new Error(`Error HTTP! Estado: ${response.status}`);
+
+const datosEventos = [
+    {
+        "img": "assets/img/practica-lademoswing.png",
+        "titulo": "Práctica Social",
+        "fecha": "Viernes 14 noviembre 21:00-22:30"
+    },
+    {
+        "img": "assets/img/web-bianca-nils-en-madrid.png",
+        "titulo": "Bianca&Nils en la Demo",
+        "fecha": "28-30 noviembre",
+        "link": "bianca-y-nils-en-madrid-info-web-la-demo.pdf"
+    },
+    {
+        "img": "assets/img/navideña-2025.png",
+        "titulo": "La Navideña",
+        "fecha": "Sábado 13 Diciembre",
+        "link": "https://www.instagram.com/p/DQ9UP-cAVLV/?img_index=1"
     }
-    return response.json(); // Convertir respuesta a objeto JS
-})
-.then(datosEventos => {
-    console.log("JSON cargado:", datosEventos);
-    generarHtml(datosEventos);   
-})  
+]
 
 
-const generarHtml =(datosEventos)=>{
+//generar HTML
+const generarHtml =()=>{
     const containerEventos = document.getElementById("events");
     /* const datosEventos = require("./eventos.json");*/
     datosEventos.forEach((evento)=>{
@@ -48,7 +57,7 @@ const generarHtml =(datosEventos)=>{
         article.classList.add("events-row");
         const link = evento.link ? `
             <div>
-                <a target="_blank" class="sign-button" href="${evento.link}">Apúntate aquí</a>
+                <a target="_blank" class="sign-button" href="${evento.link}">Más información</a>
             </div>
             ` : "";
 
@@ -71,5 +80,34 @@ const generarHtml =(datosEventos)=>{
     
 }
 
+    /* fetch('./eventos.json') // Cargar el archivo JSON
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`Error HTTP! Estado: ${response.status}`);
+                }
+                return response.json(); // Convertir respuesta a objeto JS
+            })
+            .then(data => {
+                console.log("JSON cargado:", data);
 
+                datosEventos.forEach((evento)=>{
+                    const article = document.createElement("article")
+                   // article.classList.add("events-row");
+                    const eventoHtml = `
+                        <div class="image">
+                            <img src="${evento.img}" alt="">
+                        </div>
+                        <div class="text">
+                            <h2>${evento.titulo}</h2>
+                            <div class="divider"></div>
+                            <p>${evento.fecha}</p>
+                        </div>
+                    `;
+                    article.innerHTML=eventoHtml;
+                    containerEventos.appendChild(article);
+                })
+            }) */  
     
+generarHtml();
+
+
